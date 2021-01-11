8791a7ed7a1d22aeff9c49ca1cf0fe48b05bd2da
SALIMRJ <64153790+SALIMRJ@users.noreply.github.com>
2021-01-11 15:00:30 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-  var Branches ={
+  var Seattle ={
    
-    nameLoc :'',
-    MaxCustomers :0,
-    MinCustomers :0,
-    AvgCookies :0,
+    nameLoc :'Seattle',
+    MaxCustomers :23,
+    MinCustomers :65,
+    AvgCookies :6.3,
     CookiespHour:[],
     RandomCustomers :[],
     gitRandom:function(){
@@ -17,7 +17,7 @@
     gitCookiespHouer:function(){
         console.log('gitCookiespHouer');
        for(var i =0;i<this.RandomCustomers.length;i++){
-           this.CookiespHour[i]=Math.floor(this.AvgCookies*this.RandomCustomers[i]) ;
+           this.CookiespHour[i]=Math.ceil(this.AvgCookies*this.RandomCustomers[i]) ;
        };
        this.ShowIfo();
     },
@@ -53,41 +53,256 @@
 
   };
 
+
+
+  var Tokyo ={
+   
+    nameLoc :'Tokyo',
+    MaxCustomers :3,
+    MinCustomers :24,
+    AvgCookies :1.2,
+    CookiespHour:[],
+    RandomCustomers :[],
+    gitRandom:function(){
+        console.log('gitRandom');
+        for(var i = 0;i<15;i++){
+            this.RandomCustomers[i]=GetRandom(this.MaxCustomers,this.MinCustomers);
+           
+        };
+        this.gitCookiespHouer()
+    } ,
+    gitCookiespHouer:function(){
+        console.log('gitCookiespHouer');
+       for(var i =0;i<this.RandomCustomers.length;i++){
+           this.CookiespHour[i]=Math.ceil(this.AvgCookies*this.RandomCustomers[i]) ;
+       };
+       this.ShowIfo();
+    },
+
+
+    ShowIfo:function(){
+        console.log('ShowIfo');
+        var parent =document.getElementById('SaleInfo');
+        var name = document.createElement('p');
+        name.textContent=this.nameLoc; 
+        parent.appendChild(name);
+        var listInfo=document.createElement('ul');
+        parent.appendChild(listInfo);
+        
+        var houer = 6 ;
+        var am_pm='am';
+        var total = 0; 
+        for(var i =0 ;i<this.CookiespHour.length;i++){
+            if(houer>12){
+                am_pm='pm';
+                houer=1; };
+            var liOFlu = document.createElement('li');
+            total+=this.CookiespHour[i];
+            liOFlu.textContent=houer+am_pm+' :'+this.CookiespHour[i]+' '+'cookies';
+            houer++;
+            listInfo.appendChild(liOFlu);
+        }
+        var liOFlu = document.createElement('li');
+        liOFlu.textContent='Total :'+total+' '+ 'cookies';
+        listInfo.appendChild(liOFlu);
+
+    },
+
+  };
+
+
+
+
+
+
+  var Dubai	 ={
+   
+    nameLoc :'Dubai	',
+    MaxCustomers :11,
+    MinCustomers :38,
+    AvgCookies :3.7,
+    CookiespHour:[],
+    RandomCustomers :[],
+    gitRandom:function(){
+        console.log('gitRandom');
+        for(var i = 0;i<15;i++){
+            this.RandomCustomers[i]=GetRandom(this.MaxCustomers,this.MinCustomers);
+           
+        };
+        this.gitCookiespHouer()
+    } ,
+    gitCookiespHouer:function(){
+        console.log('gitCookiespHouer');
+       for(var i =0;i<this.RandomCustomers.length;i++){
+           this.CookiespHour[i]=Math.ceil(this.AvgCookies*this.RandomCustomers[i]) ;
+       };
+       this.ShowIfo();
+    },
+
+
+    ShowIfo:function(){
+        console.log('ShowIfo');
+        var parent =document.getElementById('SaleInfo');
+        var name = document.createElement('p');
+        name.textContent=this.nameLoc; 
+        parent.appendChild(name);
+        var listInfo=document.createElement('ul');
+        parent.appendChild(listInfo);
+        
+        var houer = 6 ;
+        var am_pm='am';
+        var total = 0; 
+        for(var i =0 ;i<this.CookiespHour.length;i++){
+            if(houer>12){
+                am_pm='pm';
+                houer=1; };
+            var liOFlu = document.createElement('li');
+            total+=this.CookiespHour[i];
+            liOFlu.textContent=houer+am_pm+' :'+this.CookiespHour[i]+' '+'cookies';
+            houer++;
+            listInfo.appendChild(liOFlu);
+        }
+        var liOFlu = document.createElement('li');
+        liOFlu.textContent='Total :'+total+' '+ 'cookies';
+        listInfo.appendChild(liOFlu);
+
+    },
+
+  };
+
+  var Paris ={
+   
+    nameLoc :'Paris',
+    MaxCustomers :20,
+    MinCustomers :38,
+    AvgCookies :2.3,
+    CookiespHour:[],
+    RandomCustomers :[],
+    gitRandom:function(){
+        console.log('gitRandom');
+        for(var i = 0;i<15;i++){
+            this.RandomCustomers[i]=GetRandom(this.MaxCustomers,this.MinCustomers);
+           
+        };
+        this.gitCookiespHouer()
+    } ,
+    gitCookiespHouer:function(){
+        console.log('gitCookiespHouer');
+       for(var i =0;i<this.RandomCustomers.length;i++){
+           this.CookiespHour[i]=Math.ceil(this.AvgCookies*this.RandomCustomers[i]) ;
+       };
+       this.ShowIfo();
+    },
+
+
+    ShowIfo:function(){
+        console.log('ShowIfo');
+        var parent =document.getElementById('SaleInfo');
+        var name = document.createElement('p');
+        name.textContent=this.nameLoc; 
+        parent.appendChild(name);
+        var listInfo=document.createElement('ul');
+        parent.appendChild(listInfo);
+        
+        var houer = 6 ;
+        var am_pm='am';
+        var total = 0; 
+        for(var i =0 ;i<this.CookiespHour.length;i++){
+            if(houer>12){
+                am_pm='pm';
+                houer=1; };
+            var liOFlu = document.createElement('li');
+            total+=this.CookiespHour[i];
+            liOFlu.textContent=houer+am_pm+' :'+this.CookiespHour[i]+' '+'cookies';
+            houer++;
+            listInfo.appendChild(liOFlu);
+        }
+        var liOFlu = document.createElement('li');
+        liOFlu.textContent='Total :'+total+' '+ 'cookies';
+        listInfo.appendChild(liOFlu);
+
+    },
+
+  };
+
+
+
+  var Lima ={
+   
+    nameLoc :'Lima',
+    MaxCustomers :2,
+    MinCustomers :16,
+    AvgCookies :4.6,
+    CookiespHour:[],
+    RandomCustomers :[],
+    gitRandom:function(){
+        console.log('gitRandom');
+        for(var i = 0;i<15;i++){
+            this.RandomCustomers[i]=GetRandom(this.MaxCustomers,this.MinCustomers);
+           
+        };
+        this.gitCookiespHouer()
+    } ,
+    gitCookiespHouer:function(){
+        console.log('gitCookiespHouer');
+       for(var i =0;i<this.RandomCustomers.length;i++){
+           this.CookiespHour[i]=Math.ceil(this.AvgCookies*this.RandomCustomers[i]) ;
+       };
+       this.ShowIfo();
+    },
+
+
+    ShowIfo:function(){
+        console.log('ShowIfo');
+        var parent =document.getElementById('SaleInfo');
+        var name = document.createElement('p');
+        name.textContent=this.nameLoc; 
+        parent.appendChild(name);
+        var listInfo=document.createElement('ul');
+        parent.appendChild(listInfo);
+        
+        var houer = 6 ;
+        var am_pm='am';
+        var total = 0; 
+        for(var i =0 ;i<this.CookiespHour.length;i++){
+            if(houer>12){
+                am_pm='pm';
+                houer=1; };
+            var liOFlu = document.createElement('li');
+            total+=this.CookiespHour[i];
+            liOFlu.textContent=houer+am_pm+' :'+this.CookiespHour[i]+' '+'cookies';
+            houer++;
+            listInfo.appendChild(liOFlu);
+        }
+        var liOFlu = document.createElement('li');
+        liOFlu.textContent='Total :'+total+' '+ 'cookies';
+        listInfo.appendChild(liOFlu);
+
+    },
+
+  };
+
+
   function GetRandom (max,min){
     console.log('GetRandom');
     var Rnumber = Math.floor(Math.random()*max+1+min);
      return Rnumber ;
   };
 
-  Branches.nameLoc='Seattle';
-  Branches.MinCustomers=23;
-  Branches.MaxCustomers=65;
-  Branches.AvgCookies=6.3;
-  Branches.gitRandom();
+ 
+  Seattle.gitRandom();
   //---------------------
-  Branches.nameLoc='Tokyo';
-  Branches.MinCustomers=3;
-  Branches.MaxCustomers=24;
-  Branches.AvgCookies=1.2;
-  Branches.gitRandom();
+
+  Tokyo.gitRandom();
 //-----------------------
-Branches.nameLoc='Dubai';
-Branches.MinCustomers=11;
-Branches.MaxCustomers=38;
-Branches.AvgCookies=3.7;
-Branches.gitRandom();
+
+Dubai.gitRandom();
 //-----------------------
-Branches.nameLoc='Paris';
-Branches.MinCustomers=20;
-Branches.MaxCustomers=38;
-Branches.AvgCookies=2.3;
-Branches.gitRandom();
+
+Paris.gitRandom();
 //-----------------------
-Branches.nameLoc='Lima';
-Branches.MinCustomers=2;
-Branches.MaxCustomers=16;
-Branches.AvgCookies=4.6;
-Branches.gitRandom();   
+
+Lima.gitRandom();   
 //-----------------------
 
  
